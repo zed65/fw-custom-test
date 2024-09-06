db8c145460885a45bf6fe18c6eb86e03dc565717
GitHub git update Action <action@github.com>
2024-09-06 01:42:46 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/engine_configuration_generated_structures_ZedTest.h
+++ generated/controllers/generated/engine_configuration_generated_structures_ZedTest.h
@@ -4563,16 +4563,24 @@ struct engine_configuration_s {
 	 */
 	scaled_channel<uint8_t, 10, 1> minimumOilPressureTimeout;
 	/**
+	 * need 4 byte alignment
 	 * units: units
 	 * offset 3853
 	 */
-	uint8_t unusedOftenChangesDuringFirmwareUpdate[END_OF_CALIBRATION_PADDING];
+	uint8_t alignmentFill_at_3853[3];
 	/**
-	 * need 4 byte alignment
-	 * units: units
-	 * offset 4021
+	 * offset 3856
 	 */
-	uint8_t alignmentFill_at_4021[3];
+	linear_sensor_s auxLinear3;
+	/**
+	 * offset 3876
+	 */
+	linear_sensor_s auxLinear4;
+	/**
+	 * units: units
+	 * offset 3896
+	 */
+	uint8_t unusedOftenChangesDuringFirmwareUpdate[END_OF_CALIBRATION_PADDING];
 };
 static_assert(sizeof(engine_configuration_s) == 4024);
 
