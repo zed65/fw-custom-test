2948637e3a44bb788ed46844c2a2622a9f090699
GitHub git update Action <action@github.com>
2024-10-07 01:51:01 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_ZedTest.h
+++ generated/controllers/generated/rusefi_generated_ZedTest.h
@@ -1755,7 +1755,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.10.06.ZedTest.1487019328"
+#define TS_SIGNATURE "rusEFI main.2024.10.07.ZedTest.1487019328"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
